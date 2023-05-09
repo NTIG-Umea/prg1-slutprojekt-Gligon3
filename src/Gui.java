bebc0ab561ba91637f9badd3cf5c39dbffc3ec12
Gligon3 <94972508+Gligon3@users.noreply.github.com>
2023-05-09 15:05:54 +0200

    Fixade in 1 pjäs men  det blev problem

--- src/Gui.java
+++ src/Gui.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Gui {
+    private JPanel Chess;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gui");
+        frame.setContentPane(new Gui().Chess);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
